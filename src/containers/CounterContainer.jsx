2bfe94fe8f347324c39c35db23b482500fb4b7d3
Your Name <you@example.com>
2019-11-25 16:26:43 +0530

    CoimbineReducers with mapstate to Prop

--- src/containers/CounterContainer.jsx
+++ src/containers/CounterContainer.jsx
@@ -2,6 +2,8 @@ import { connect } from 'react-redux';
 import React, { Component } from 'react';
 import { INCREMENT } from '../actioncontants';
 import { CounterView } from '../views/CounterView';
+import { CounterReducer } from '../reducers';
+
 
 class Counter extends Component {
 
@@ -18,7 +20,9 @@ class Counter extends Component {
 //mapper function : it is function which converts state into prop.
 function mapStateToProp(state) {
       //It returns configuration Object
-      let count = state.count;
+      //let count = state.count;
+      //let count = state.reducer.reducerdata
+      let count = state.CounterReducer.count;
       //key would be react prop,value would be redux state
       return {
             //key:value
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import React from 'react';
 import { render } from 'react-dom'
 import { Provider } from 'react-redux';
-import { CounterReducer } from './reducers';
+import { CounterReducer, CartReducer } from './reducers';
 import { CounterContainer } from './containers'
 import 'bootstrap/dist/css/bootstrap.css';
 
 //create store 
-const appStore = createStore(CounterReducer);
+const rootReducer = combineReducers({ CounterReducer, CartReducer });
+const appStore = createStore(rootReducer);
 
 const App = () => {
       return <Provider store={appStore}>
