0d443d62de37b8d015471b05a7ab052d33393441
Your Name <you@example.com>
2019-11-25 17:04:26 +0530

    api call with redux

--- src/index.js
+++ src/index.js
@@ -1,33 +1,84 @@
 import {
-      createStore
+      createStore, applyMiddleware
 } from 'redux';
+import thunk from 'redux-thunk';
 
 const FINDALL = 'FINDALL'
 
 const todoReducer = (state = [], action) => {
       switch (action.type) {
             case FINDALL:
-                  return [...state, action.todos];
+                  return state.concat(action.todos)
             default:
                   return state;
       }
 }
-const store = createStore(todoReducer);
 
-//action creators
-function findAllTodos(todos) {
+const Logger = store => next => action => {
+      // your middleware-specific code goes here
+      console.log('dispatching LOGGER', action)
+      let result = next(action)
+      console.log(store.getState())
+      return result
+};
+const middlewareConfig = applyMiddleware(Logger, thunk)
+
+const store = createStore(todoReducer, middlewareConfig);
+
+store.subscribe(function () {
+      console.log(store.getState());
+})
+
+//sync action creators
+/* function findAllTodos(todos) {
+      return {
+            type: FINDALL,
+            todos
+      }
+
+} */
+/* function findAllTodos(todos) {
       setTimeout(function () {
-            //after 2000ms return action object
             return {
                   type: FINDALL,
                   todos
             }
       }, 2000)
 
+} */
+//without middleware
+/* function findAllTodos() {
+      const url = 'https://jsonplaceholder.typicode.com/todos'
+      fetch(url).then(res => res.json()).then(todos => {
+            return {
+                  type: FINDALL,
+                  todos
+            }
+      });
+} */
+
+//with middlware 
+
+
+function findAllTodos() {
+      return function (dispatch) {
+            const url = 'https://jsonplaceholder.typicode.com/todos'
+            fetch(url).then(res => res.json()).then(todos => {
+                  //dispatch never be called until async operation is completed
+                  dispatch({
+                        type: FINDALL,
+                        todos
+                  })
+            });
+      }
+
 }
-store.dispatch(findAllTodos({
-      "userId": 1,
-      "id": 1,
-      "title": "delectus aut autem",
-      "completed": false
-}))
+
+// store.dispatch(findAllTodos({
+//       "userId": 1,
+//       "id": 1,
+//       "title": "delectus aut autem",
+//       "completed": false
+// }))
+//this should go inside componentDidMount
+store.dispatch(findAllTodos());
